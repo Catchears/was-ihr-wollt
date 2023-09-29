fe4b6c816e94e94daeeb382d370774a1473df8fb
Catchears <catchears@gmail.com>
2023-09-30 00:10:55 +0200

    reorganize layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,19 +7,14 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      Source: "https://github.com/Catchears/was-ihr-wollt",
+      Quelltext: "https://github.com/Catchears/was-ihr-wollt",
     },
   }),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs({ rootName: "Was Ihr Wollt", resolveFrontmatterTitle: true }),
-    Component.ArticleTitle(),
-    Component.NearbyScenes(),
-    Component.TagList(),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.NearbyScenes()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -38,42 +33,52 @@ export const defaultContentPageLayout: PageLayout = {
     ),
   ],
   right: [
-    Component.Graph({
-      localGraph: {
-        drag: true, // whether to allow panning the view around
-        zoom: true, // whether to allow zooming in and out
-        depth: 1, // how many hops of notes to display
-        scale: 2, // default view scale
-        repelForce: 0.7, // how much nodes should repel each other
-        centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 40, // how long should the links be by default?
-        fontSize: 0.5, // what size should the node labels be?
-        opacityScale: 4, // how quickly do we fade out the labels when zooming out?
-      },
-      globalGraph: {
-        drag: true,
-        zoom: true,
-        depth: -1,
-        scale: 0.9,
-        repelForce: 0.7,
-        centerForce: 0.3,
-        linkDistance: 45,
-        fontSize: 0.6,
-        opacityScale: 1,
-      },
-    }),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.DesktopOnly(Component.Backlinks()),
+    Component.DesktopOnly(
+      Component.Graph({
+        localGraph: {
+          drag: true, // whether to allow panning the view around
+          zoom: true, // whether to allow zooming in and out
+          depth: 1, // how many hops of notes to display
+          scale: 2, // default view scale
+          repelForce: 0.7, // how much nodes should repel each other
+          centerForce: 0.3, // how much force to use when trying to center the nodes
+          linkDistance: 40, // how long should the links be by default?
+          fontSize: 0.5, // what size should the node labels be?
+          opacityScale: 4, // how quickly do we fade out the labels when zooming out?
+        },
+        globalGraph: {
+          drag: true,
+          zoom: true,
+          depth: -1,
+          scale: 0.9,
+          repelForce: 0.7,
+          centerForce: 0.3,
+          linkDistance: 45,
+          fontSize: 0.6,
+          opacityScale: 1,
+        },
+      }),
+    ),
+    Component.DesktopOnly(
+      Component.Breadcrumbs({
+        rootName: "Was Ihr Wollt",
+        resolveFrontmatterTitle: true,
+        hideOnRoot: false,
+      }),
+    ),
     Component.MobileOnly(
       Component.Explorer({
+        title: "Übersicht",
         mapFn: (node) => {
-          node.name = node.displayName = node.displayName
+          node.displayName = node.displayName
             .replace("act", "Akt")
             .replace("-", " ")
             .replace("figuren", "Figuren")
         },
       }),
     ),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.Backlinks()),
   ],
 }
 
