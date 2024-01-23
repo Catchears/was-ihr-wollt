34c0fc2c829898cf3e4ad4951f956d02c9065278
Catchears <catchears@gmail.com>
2024-01-23 23:13:46 +0100

    disable external link icon

--- quartz.config.ts
+++ quartz.config.ts
@@ -53,7 +53,7 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest", externalLinkIcon: false }),
       Plugin.Description(),
     ],
     filters: [Plugin.RemoveDrafts()],
